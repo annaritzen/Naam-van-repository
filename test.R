d2f51215dfe5f2f39e818677b898c69c32d5e86e
annaritzen <annaritzen.1994@gmail.com>
2020-09-23 19:50:25 +0200

    beschrijving code

---
+++ test.R
@@ -0,0 +1,8 @@
+data("marketing", package = "datarium")
+
+ggplot(marketing, aes(x = youtube, y = sales)) +
+  geom_point() +
+  stat_smooth()
+
+model <- lm(sales ~ youtube, data = marketing)
+model
